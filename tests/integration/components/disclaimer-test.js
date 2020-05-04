1945423d61070a08808b14c13543cde653e75636
Craig Brentz <brentzc@gmail.com>
2020-05-03 19:23:24 -0500

    added disclaimer component

---
+++ tests/integration/components/disclaimer-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | disclaimer', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<Disclaimer />`);
+
+    assert.equal(this.element.textContent.trim(), '');
+
+    // Template block usage:
+    await render(hbs`
+      <Disclaimer>
+        template block text
+      </Disclaimer>
+    `);
+
+    assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+});
